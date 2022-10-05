7a8ac4f913dc71660b58a696ea7eb23820e88ba5
Apina-32 <samuli.ruuskanen@tuni.fi>
2022-10-05 06:12:42 +0300

    Update GitHub Fork commits.user.js

--- GitHub Fork commits.user.js
+++ GitHub Fork commits.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         GitHub Fork commits
-// @namespace    https://github.com/Apina-32/
+// @namespace    https://a32.fi
 // @version      0.2
 // @description  checks if fork is ahead
 // @author       Apina-32
 // @updateURL    https://github.com/Apina-32/userscripts/raw/main/GitHub%20Fork%20commits.user.js
+// @downloadURL  https://github.com/Apina-32/userscripts/raw/main/GitHub%20Fork%20commits.user.js
 // @match        https://github.com/*
 // @icon         https://icons.duckduckgo.com/ip2/github.com.ico
 // @grant        GM_registerMenuCommand
