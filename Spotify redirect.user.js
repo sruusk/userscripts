2485bdb478dc0641896d8ff9b5ecdad8aedd9d3a
Apina-32 <samuli.ruuskanen@tuni.fi>
2022-10-05 06:11:47 +0300

    Update Spotify redirect.user.js

--- Spotify redirect.user.js
+++ Spotify redirect.user.js
@@ -3,7 +3,7 @@
 // @version      1.2
 // @namespace    https://a32.fi
 // @updateURL    https://github.com/Apina-32/userscripts/raw/main/Spotify%20redirect.user.js
-// @downlaodURL  https://github.com/Apina-32/userscripts/raw/main/Spotify%20redirect.user.js
+// @downloadURL  https://github.com/Apina-32/userscripts/raw/main/Spotify%20redirect.user.js
 // @match        http://open.spotify.com/*
 // @match        https://open.spotify.com/*
 // @grant        window.close
